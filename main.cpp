d1670091c17c0315bb7a7e7cd5b28df22efcf4ea
leonardumale <leonardumale@gmail.com>
2026-04-23 19:03:27 -0700

    Maybe Final

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
-// idk man
+#include <iostream>
+#include <fstream>
+#include "getinput.hpp"
+#include "calc.hpp"
+#include "print.hpp" 
+int main() {
+	Student s;
+  getStudentInfo(&s);
+  int result = mainCalculation();
+  if (result != 0) {
+    std::cout << "Error: something wrong with caculations"
+      return 1;
+  }
+std::ifstream gpaFile("gpa.txt");
+if (!gpaFile) {
+  std::cout << " Error: could not open gpa.txt\n";
+return 1;
+}
+gpaFile >> s.gpa;
+gpaFile.close();
+printResults(&s);
+return 0;
+}
