e2eec1fbc5a32fe69f5cce8c77ac80798bf7e9df
leonardumale <leonardumale@gmail.com>
2026-04-23 18:39:13 -0700

    Add include guards and GPA calculation function
    Added include guards and necessary includes for GPA calculation.

--- print.hpp
+++ print.hpp
@@ -1,1 +1,10 @@
-
+#ifndef PRINT_HPP
+#define PRINT_HPP
+#include <iostream>
+#include <string>
+#include "getinput.hpp"
+inline float calculateGPA (const std::string& filename)
+{
+  std::ifstream in File(filename);
+  if (!inFile) {
+    std::cerr << "Error opening" 
